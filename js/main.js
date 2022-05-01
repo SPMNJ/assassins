7fcf8f5d6f657ae662d74a1fe638700036f560b8
Sean McGuire <16692089+SPMNJ@users.noreply.github.com>
2022-05-01 19:53:48 -0400

    PUSH IT

--- js/main.js
+++ js/main.js
@@ -1,35 +1,70 @@
-var url = "";
+var url = "https://script.google.com/macros/s/AKfycbwVDyr-tt7Kc7G3xMtTBhyxAdQWvghG4Q5RlR4rE1vtj1PHdBBB3SfVgrc7Wzp4pXqq/exec";
 (function ($) {
-    "use strict";
-	$.ajaxSetup({
-crossDomain: true,
-	type: "GET",
-	dataType: "jsonp",
-});
-    /*==================================================================
-    [ Focus input ]*/
+  "use strict";
+  $.ajaxSetup({
+    crossDomain: true,
+    type: "GET",
+    dataType: "jsonp",
+  });
+  /*==================================================================
+  [ Focus input ]*/
 
 
-    /*==================================================================
-    [ Validate ]*/
-// Bind to the submit event of our form
-$(document).on('submit',"#searchForm",function(event){
-	event.preventDefault();
-	$('#info').hide();
-	$('#info').html("");
+  /*==================================================================
+  [ Validate ]*/
+  // Bind to the submit event of our form
+  $(document).on('click', "#load", function (event) {
+    event.preventDefault();
+    $('#info').hide();
+    $('#info').html("");
     $(".btn-loading").show();
-	$(".search-form-btn").hide();
-	var input = $("#searchbox").val();
-	$.ajax({
-		url: url + "?type=search&q="+input, 		
-});
+    $(".search-form-btn").hide();
+    var input = $("#searchbox").val();
+    $.ajax({
+      url: url + "?type=user&name=" + input,
+    });
 
-	return false; 
-});
+    return false;
+  });
 
-	
+
 })(jQuery);
 
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (const i of ca) {
+    var c = i;
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {
+  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function user(response) {
+  if (response.status == 200) {
+    setCookie('id', response.message, 5);
+    window.location.href = "kill.html";
+  }
+}
+
+function kill(response) {
+  if (response.status == 200) {
+    $("#target").html(response.message);
+  }
+}
 
 function getAllUrlParams(url) {
   var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
@@ -55,7 +90,7 @@ function getAllUrlParams(url) {
       } else {
         if (!obj[paramName]) {
           obj[paramName] = paramValue;
-        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+        } else if (obj[paramName] && typeof obj[paramName] === 'string') {
           obj[paramName] = [obj[paramName]];
           obj[paramName].push(paramValue);
         } else {
